973d5b1f90724f5dba15f95a440f915f105cf2fd
mcchan1995 <95762560+mcchan1995@users.noreply.github.com>
2022-05-03 20:49:19 +0800

    Update dash.py

--- dash.py
+++ dash.py
@@ -6,15 +6,79 @@ import plotly.express as px
 import pymongo
 from bson.objectid import ObjectId
 import plotly.graph_objs as go
+import dash_bootstrap_components as dbc
 
-# myclient = pymongo.MongoClient("mongodb://ia.dsa.21.a:tuJ6ZdJGWrEf8SAd6gb8ZaHUcs83HHJu@18.189.210.178:27017/?authSource=IA&readPreference=primary&directConnection=true&ssl=false")
-# mydb = myclient["Ukdata"]
-# mycol = mydb["Crime"]
+myclient = pymongo.MongoClient("mongodb://ia.dsa.21.a:tuJ6ZdJGWrEf8SAd6gb8ZaHUcs83HHJu@18.189.210.178:27017/?authSource=IA&readPreference=primary&directConnection=true&ssl=false")
+mydb = myclient["Ukdata"]
+mycol = mydb["Crime"]
 
 app = dash.Dash()
 
 server = app.server
 
+
+external_stylesheets = [dbc.themes.BOOTSTRAP]
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = 'UK Dashboard'
+
+#########header#########
+colors = {
+    'background': '#111111',
+    'bodyColor':'#F2DFCE',
+    'text': '#7FDBFF'
+}
+def get_page_heading_style():
+    return {'backgroundColor': colors['background']}
+
+def get_page_heading_title():
+    return html.H1(children='UK Crime Dashboard',
+                                        style={
+                                        'textAlign': 'center',
+                                        'color': colors['text']
+                                    })
+
+def get_page_heading_subtitle():
+    return html.Div(children='Count crime cases from each region',
+                                         style={
+                                             'textAlign':'center',
+                                             'color':colors['text']
+                                         })
+
+def generate_page_header():
+    main_header =  dbc.Row(
+                            [
+                                dbc.Col(get_page_heading_title(),md=12)
+                            ],
+                            align="center",
+                            style=get_page_heading_style()
+                        )
+    subtitle_header = dbc.Row(
+                            [
+                                dbc.Col(get_page_heading_subtitle(),md=12)
+                            ],
+                            align="center",
+                            style=get_page_heading_style()
+                        )
+    header = (main_header,subtitle_header)
+    return header
+
+
+#################################################################################################
+class RealizedProfitLoss:
+    def __init__(self,df):
+        self.dataframe = df
+
+    def plot (self,start_date = None, end_date = None):
+        df = self.dataframe
+        if start_date:
+            df = df[df['date'] >= start_date]
+        if end_date:
+            df = df[df['date'] <= end_date]
+
+    def run_dash(self):
+        app = JupyterDash(__name__)
+
+######################################################################
 app.layout = html.Div([
     dcc.Graph(id='map-with-slider'),
     dcc.Slider(
@@ -31,18 +95,3 @@ app.layout = html.Div([
     Output('map-with-slider','figure'),
     [Input('year-slider','value')]
 )
-#################################################################################################
-class RealizedProfitLoss:
-    def __init__(self,df):
-        self.dataframe = df
-
-    def plot (self,start_date = None, end_date = None):
-        df = self.dataframe
-        if start_date:
-            df = df[df['date'] >= start_date]
-        if end_date:
-            df = df[df['date'] <= end_date]
-
-    def run_dash(self):
-        app = JupyterDash(__name__)
-
