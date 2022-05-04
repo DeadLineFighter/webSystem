aff22d0639d0870e3eea773c3a83850cc2aab055
mcchan1995 <95762560+mcchan1995@users.noreply.github.com>
2022-05-04 21:39:05 +0800

    update dash

--- dash.py
+++ dash.py
@@ -1,12 +1,13 @@
-from model import db
+from model.db import db
 import dash
-from dash import html,dcc,Input,Output,State,dash_table
 import pandas as pd
 import plotly.express as px
 import pymongo
-from bson.objectid import ObjectId
+import os
+# import dash_html_components as html
 import plotly.graph_objs as go
 import dash_bootstrap_components as dbc
+
 
 myclient = pymongo.MongoClient("mongodb://ia.dsa.21.a:tuJ6ZdJGWrEf8SAd6gb8ZaHUcs83HHJu@18.189.210.178:27017/?authSource=IA&readPreference=primary&directConnection=true&ssl=false")
 mydb = myclient["Ukdata"]
@@ -18,62 +19,75 @@ app.title = 'UK Dashboard'
 
 
 #########header#########
-colors = {
-    'background': '#111111',
-    'bodyColor':'#F2DFCE',
-    'text': '#7FDBFF'
-}
-def get_page_heading_style():
-    return {'backgroundColor': colors['background']}
+# colors = {
+#     'background': '#111111',
+#     'bodyColor':'#F2DFCE',
+#     'text': '#7FDBFF'
+# }
+# def get_page_heading_style():
+#     return {'backgroundColor': colors['background']}
 
-def get_page_heading_title():
-    return html.H1(children='UK Crime Dashboard',
-                                        style={
-                                        'textAlign': 'center',
-                                        'color': colors['text']
-                                    })
+# def get_page_heading_title():
+#     return html.H1(children='UK Crime Dashboard',
+#                                         style={
+#                                         'textAlign': 'center',
+#                                         'color': colors['text']
+#                                     })
 
-def get_page_heading_subtitle():
-    return html.Div(children='Count crime cases from each region',
-                                         style={
-                                             'textAlign':'center',
-                                             'color':colors['text']
-                                         })
+# def get_page_heading_subtitle():
+#     return html.Div(children='Count crime cases from each region',
+#                                          style={
+#                                              'textAlign':'center',
+#                                              'color':colors['text']
+#                                          })
 
-def generate_page_header():
-    main_header =  dbc.Row(
-                            [
-                                dbc.Col(get_page_heading_title(),md=12)
-                            ],
-                            align="center",
-                            style=get_page_heading_style()
-                        )
-    subtitle_header = dbc.Row(
-                            [
-                                dbc.Col(get_page_heading_subtitle(),md=12)
-                            ],
-                            align="center",
-                            style=get_page_heading_style()
-                        )
-    header = (main_header,subtitle_header)
-    return header
+# def generate_page_header():
+#     main_header =  dbc.Row(
+#                             [
+#                                 dbc.Col(get_page_heading_title(),md=12)
+#                             ],
+#                             align="center",
+#                             style=get_page_heading_style()
+#                         )
+#     subtitle_header = dbc.Row(
+#                             [
+#                                 dbc.Col(get_page_heading_subtitle(),md=12)
+#                             ],
+#                             align="center",
+#                             style=get_page_heading_style()
+#                         )
+#     header = (main_header,subtitle_header)
+#     return header
         
 ########View#######
-app.layout = html.Div([
-    dcc.Graph(id='map-with-slider'),
-    dcc.Slider(
-        id='year-slider',
-        min= 2017,
-        max = 2020,
-        value=2017,
-        step=1,
-        marks={year:str(year) for year in [2017,2018,2019,2020]}
-    )
-])
+# app.layout = html.Div([
+#     dcc.Graph(id='map-with-slider'),
+#     dcc.Slider(
+#         id='year-slider',
+#         min= 2017,
+#         max = 2020,
+#         value=2017,
+#         step=1,
+#         marks={year:str(year) for year in [2017,2018,2019,2020]}
+#     )
+# ])
+trace = go.Bar(x=db.monthCrime.crime, y=db.monthCrime.count, name='crime')
 
+app.layout = html.Div(children=[html.H1("CData Extension + Dash", style={'textAlign': 'center'}),
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [trace],
+            'layout':
+            go.Layout(title='UK crime Data', barmode='stack')
+        })
+], className="container")
 
 ########Controller#######
-@app.callback(
-    Output('map-with-slider','figure'),
-    [Input('year-slider','value')])
-def map_from_uk(year):
+# @app.callback(
+#     Output('map-with-slider','figure'),
+#     [Input('year-slider','value')])
+# def map_from_uk(year):
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
