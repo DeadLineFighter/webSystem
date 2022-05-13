984b88bf9aa52c6afb44f40f0abc5357e264d913
Sunny Ho <sunny.ho.r2c2@gmail.com>
2022-05-14 03:56:50 +0800

    feat: add dash function caller to main

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__))+"\\model")
 template_dir = os.path.dirname(os.path.abspath(__file__))+"\\template"
 from website.views import views
 from website.auth import auth
-
+from dashboard import dashboard
 server = Flask(__name__, template_folder=template_dir)
 
 server.register_blueprint(views, url_prefix='/')
 server.register_blueprint(auth, url_prefix='/')
+app = dashboard(server)
 
 if __name__ == '__main__':
     server.run(debug=False)
