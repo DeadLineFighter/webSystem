123f14180c5ac1ea57a51474fdeed8e60f8c9a69
Sunny Ho <sunny.ho.r2c2@gmail.com>
2022-05-15 18:41:26 +0800

    delete: repeated variable

--- app.py
+++ app.py
@@ -4,12 +4,11 @@ import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__))+"\\model")
 template_dir = os.path.dirname(os.path.abspath(__file__))+"\\template"
-static_dir = os.path.dirname(os.path.abspath(__file__))+"\\template"
 
 from website.views import views
 from website.auth import auth
 
-server = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+server = Flask(__name__, template_folder=template_dir, static_folder=template_dir)
 
 server.register_blueprint(views, url_prefix='/')
 server.register_blueprint(auth, url_prefix='/')
